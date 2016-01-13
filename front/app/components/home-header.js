4721c0205fe19038f14d61912a6d07929074841b
flamen <jitang.zheng@gmail.com>
2016-01-13 12:51:45 -0800

    switch languages

---
+++ front/app/components/home-header.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+
+export default Ember.Component.extend({
+  i18n: Ember.inject.service(),
+  locales: Ember.computed('i18n.locale', 'i18n.locales', function() {
+    const i18n = this.get('i18n');
+    return this.get('i18n.locales').map(function (loc) {
+      return { id: loc, text: i18n.t('locales.' + loc) };
+    });
+  }),
+  actions: {
+    setLocale(locale) {      
+      this.set('i18n.locale', locale);
+    }
+  }
+});
---
+++ front/app/locales/en-us/translations.js
@@ -0,0 +1,36 @@
+export default {
+  locales:{
+    "en-us":"English",
+    "zh-cn": "简体中文"
+  },
+  placeholders:{
+    search: "Please click here to search."
+  },
+  buttons:{
+    ok:"Ok",
+    cancel: "Cancel",
+    close: "Close",
+    yes: "Yes",
+    no: "No",
+    submit: "Submit",
+    reset: "Reset",
+    show: "Show",
+    remove: "Remove",
+    create: "Create",
+    edit: "Edit",
+    update: "Update",
+    upgrade: "Upgrade",
+    apply: "Apply",
+    search: "Search"
+  },
+  build_using: "Build using <a href='{{url}}' target='_blank'>{{url}}</a>"
+  // "some.translation.key": "Text for some.translation.key",
+  //
+  // "a": {
+  //   "nested": {
+  //     "key": "Text for a.nested.key"
+  //   }
+  // },
+  //
+  // "key.with.interpolation": "Text with {{anInterpolation}}"
+};
