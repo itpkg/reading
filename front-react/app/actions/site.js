667d1e1709ab54d5959455f636e22700cd6c5a1e
Jitang Zheng <jitang.zheng@gmail.com>
2016-01-27 09:16:15 -0800

    fix document.title bug

--- front-react/app/actions/site.js
+++ front-react/app/actions/site.js
@@ -1,6 +1,7 @@
 import {FETCH_SITE_INFO} from '../constants'
 
 export function refresh(info) {
+    document.title = info.subTitle + '-' + info.title;
     return {
         type: FETCH_SITE_INFO,
         info
